da65141d1db5218e3a110ea23792534fec9f96c8
Embrey Morton <emorton@unc.edu>
2023-02-26 16:02:47 -0500

    Initial commit

---
+++ tweetList.py
@@ -0,0 +1,22 @@
+from text import *
+from media_paths import *
+
+# Define the tweet arguments as a list of tuples
+tweet_args = [
+    (top10,),
+    (switchTeam,),
+    (switchTeam,),
+    (retireJersey,),
+    (imBetter,),
+    (isBetter,),
+    (teamUp,),
+    (hardest,),
+    (wojBomb, IMAGE_COOK),
+    (itainteasy, IMAGE_ITAINEASY),
+    (wemby, IMAGE_WEMBY),
+    (heTrash, VIDEO_TRASH),
+    (yachtyMeme, VIDEO_YACHTY),
+    (shaqandkobe, VIDEO_SHAQKOBE),
+    (twoMore, VIDEO_TWOMORE),
+    (doong, VIDEO_DUCK)
+]
