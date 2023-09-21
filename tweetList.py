96705189ad9975c7592039ab7612b112adc32149
embreymorton <111366508+embreymorton@users.noreply.github.com>
2023-09-21 00:37:31 -0400

    add .gitignore

--- tweetList.py
+++ tweetList.py
@@ -2,21 +2,28 @@ from text import *
 from media_paths import *
 
 # Define the tweet arguments as a list of tuples
-tweet_args = [
+tweets = [
+    (mvpTweet,),
     (top10,),
-    (switchTeam,),
     (switchTeam,),
     (retireJersey,),
     (imBetter,),
     (isBetter,),
     (teamUp,),
     (hardest,),
+    (awardRace,),
+    (gamePrediction,),
+    (rookieTooGood,),
+
+    # Image Tweets
     (wojBomb, IMAGE_COOK),
     (itainteasy, IMAGE_ITAINEASY),
     (wemby, IMAGE_WEMBY),
-    (heTrash, VIDEO_TRASH),
-    (yachtyMeme, VIDEO_YACHTY),
-    (shaqandkobe, VIDEO_SHAQKOBE),
-    (twoMore, VIDEO_TWOMORE),
-    (doong, VIDEO_DUCK)
+
+    # Video Tweets
+    # (heTrash, VIDEO_TRASH),
+    # (yachtyMeme, VIDEO_YACHTY),
+    # (shaqandkobe, VIDEO_SHAQKOBE),
+    # (twoMore, VIDEO_TWOMORE),
+    # (doong, VIDEO_DUCK)
 ]
